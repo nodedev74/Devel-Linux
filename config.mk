84bc9625bcba6bb9b15086b5f6ae4b0ff8451e7f
nodedev74 <buelenard@tutanota.com>
2024-09-01 05:08:06 -1000

    example config.mk init

---
+++ config.mk
@@ -0,0 +1,10 @@
+# Config v1
+
+VERSION				= 0.1
+NAME				= devel-linux
+LABEL				= Devel Linux	
+PUBLISHER			= nodedev74 <buelenard@tutanota.com>
+
+USERNAME			= nodedev
+TIMEZONE			= Europe/Berlin
+KEYBOARD_LAYOUT		= de
