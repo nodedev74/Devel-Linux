a115806c621268997769949cd0a570dc041b9786
nodedev74 <buelenard@tutanota.com>
2024-09-05 09:10:33 -1000

    first build impl

--- Makefile
+++ Makefile
@@ -3,4 +3,11 @@
 include config.mk
 
 bootstrap:
-	scripts/bootstrap.sh
+	bash scripts/bootstrap.sh
+
+build:
+	bash scripts/build.sh $(NAME) $(LABEL) $(PUBLISHER) $(USERNAME) $(LOCALES) $(TIMEZONE) $(KEYBOARD_LAYOUTS)
+
+test:
+
+install:
---
+++ scripts/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+if [ id -u = 0 ];
+then
+    echo ""
+    exit 1
+fi
+
+ISO_APPLICATION_NAME="$1"
+ISO_APPLICATION_LABEL="$2"
+ISO_PUBLISHER="$3"
+
+LIVE_USERNAME="$4"
+LIVE_LOCALES="$5"
+LIVE_TIMEZONE="$6"
+LIVE_KEYBOARD_LAYOUT="$7"
+
+sudo lb clean
+
+lb config noauto \
+    -d bookworm \
+    --image-name "$ISO_APPLICATION_NAME" \
+    --iso-application "$ISO_APPLICATION_LABEL" \
+    --iso-publisher "$ISO_PUBLISHER" \
+    --debian-installer none \
+    --archive-areas "main contrib non-free-firmware" \
+    --debootstrap-options "--variant=minbase --include=apt-transport-https,gnupg,openssl" \
+    --bootappend-live "boot=live components username=$LIVE_USERNAME locales=$LIVE_LOCALES timezone=$LIVE_TIMEZONE keyboard-layouts=$LIVE_KEYBOARD_LAYOUT"
+
+sudo lb build
