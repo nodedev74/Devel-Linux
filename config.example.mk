59309fa5e297d10b692e7ca8e6edf3d5c48f60fe
nodedev74 <87693559+nodedev74@users.noreply.github.com>
2024-09-11 13:24:31 +0200

    Update config.example.mk

--- config.example.mk
+++ config.example.mk
@@ -5,7 +5,12 @@ NAME				      = devel-linux
 LABEL				      = "Devel Linux"	
 PUBLISHER			    = "nodedev74 <buelenard@tutanota.com>"
 
+# Live environment
+
 USERNAME			    = nodedev
 LOCALES				    = en_US.UTF-8
 TIMEZONE			    = UTC
 KEYBOARD_LAYOUTS	= en
+
+# Obsidian plugins
+
