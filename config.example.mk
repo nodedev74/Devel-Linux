2b332d4d57d3d38891d40a2c3148d418ba4524da
nodedev74 <87693559+nodedev74@users.noreply.github.com>
2024-09-11 11:13:32 +0200

    Update config.example.mk

--- config.example.mk
+++ config.example.mk
@@ -1,11 +1,11 @@
 # Example Config v1
 
-VERSION				= 0.1
-NAME				= devel-linux
-LABEL				= "Devel Linux"	
-PUBLISHER			= "nodedev74 <buelenard@tutanota.com>"
+VERSION				    = 0.1
+NAME				      = devel-linux
+LABEL				      = "Devel Linux"	
+PUBLISHER			    = "nodedev74 <buelenard@tutanota.com>"
 
-USERNAME			= nodedev
-LOCALES				= en_US.UTF-8
-TIMEZONE			= UTC
+USERNAME			    = nodedev
+LOCALES				    = en_US.UTF-8
+TIMEZONE			    = UTC
 KEYBOARD_LAYOUTS	= en
